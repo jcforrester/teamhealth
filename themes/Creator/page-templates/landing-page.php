0016ec69bead44b19d1b7f0262d68b457aeadf16
Josh <twicearoundmars@yahoo.com>
2018-02-02 21:31:59 -0500

    Moved to template and created mod sass file

---
+++ themes/Creator/page-templates/landing-page.php
@@ -0,0 +1,59 @@
+<?php 
+/**
+ * Template Name: Landing Page
+ * 
+ */
+
+get_header(); ?>
+
+<div class="flex-container">
+    <div class="container">
+	<div class="blurb">
+		<img src="http://via.placeholder.com/250x150">
+		<p>1 in 5 Residents</p>
+		<div class="overlay">
+			<div class="text">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus turpis purus, blandit iaculis elit iaculis utNullam commodo.
+			</div>
+			<i class="fas fa-times"></i>
+		</div>
+	</div>	
+
+
+
+	<div class="blurb">	
+		<img src = "http://via.placeholder.com/250x150">
+		<p>Lose The Loans</p>
+		<div class="overlay">
+			<div class = "text">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus turpis purus, blandit iaculis elit iaculis utNullam commodo.
+			</div>
+			<i class="fas fa-times"></i>
+		</div>
+	</div>	
+
+	<div class="blurb">	
+		<img src = "http://via.placeholder.com/250x150">
+		<p>Why TeamHealth?</p>
+		<div class="overlay">
+			<div class = "text">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus turpis purus, blandit iaculis elit iaculis utNullam commodo.
+			</div>
+			<i class="fas fa-times"></i>
+		</div>
+	</div>	
+
+	<div class="blurb">	
+		<img src = "http://via.placeholder.com/250x150">
+		<p>More Practice, More Pay</p>
+		<div class="overlay">
+			<div class = "text">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus turpis purus, blandit iaculis elit iaculis utNullam commodo.
+			</div>
+			<i class="fas fa-times"></i>
+		</div>
+	</div>	
+</div>
+
+<?php 
+get_footer();
